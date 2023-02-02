bd328c193ace55a37a3be375664da2344d739455
Leon Si <leon@leondreamed.com>
2023-02-02 11:53:32 -0500

    chore: update counters config

---
+++ examples/counters.config.mjs
@@ -0,0 +1,47 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	logServerEvents: true,
+	servicesToLog: {
+		counter1: true,
+		counter2: true,
+		counter3: true,
+	},
+	services: {
+		counter1: {
+			startAutomatically: true,
+			command: {
+				string: `
+					awk 'BEGIN {
+						for (i=1; ; i++) {
+							print i; system("sleep 0.05")
+						}
+					}'
+				`,
+			},
+		},
+		counter2: {
+			startAutomatically: true,
+			command: {
+				string: `
+					awk 'BEGIN {
+						for (i=1000; ; i++) {
+							print i; system("sleep 1")
+						}
+					}'
+				`,
+			},
+		},
+		counter3: {
+			startAutomatically: true,
+			command: {
+				string: `
+					awk 'BEGIN {
+						for (i=1000000; ; i++) {
+							print i; system("sleep 5")
+						}
+					}'
+				`,
+			},
+		},
+	},
+}
